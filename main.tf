32b52d4e8dcfead73aacecc03befef1a133b4fa0
rafa-fortes <r.s.neves@hotmail.com>
2022-06-29 18:40:26 -0300

    adiciona primeira instancia na aws

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.1.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d70546e43a941d70"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Primeira instance"
+  }
+}
