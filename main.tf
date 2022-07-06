3cad56c3dd6c3097ca12bc2307df826d9ff7776e
rafa-fortes <r.s.neves@hotmail.com>
2022-07-06 18:47:22 -0300

    Projeto final

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0d70546e43a941d70"
   instance_type = "t2.micro"
-
+  key_name      = "rafael-oregon"
+  #user_data = <<-EOF
+  #                !/bin/bash
+  #               cd /home/ubuntu
+  #               echo "<h1>Feito com Terraform</h1>" > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #               EOF
   tags = {
-    Name = "Primeira instance"
+    Name = "Terraform, Ansible e Python"
   }
 }
